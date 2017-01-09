706ab8de7df3f201902b4a0f59255491b634fa86
Darla Anne Linan <darlaannelinan@Darlas-MacBook-Pro.local>
2017-01-09 15:30:04 -0800

    add photo drop down menu

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,21 @@
 })(function ($, window, document) {
 	"use strict";
 
-	// JS goes here
+	// photo container elements
+	var $addPhotoButton = $('#addPhoto button'),
+		$photoDropDown = $('#photoDropDown'),
+		$photoDropDownLink = $('#photoDropDown a'),
+		$addPhotoButtonCaret = $('#addPhoto > button > img');
+	
+	// toggle slide menu for adding photo
+	$addPhotoButton.on("click", function () {
+		$addPhotoButtonCaret.toggleClass('caretUp');
+		$photoDropDown.slideToggle(250);
+	});
+
+	// toggle menu closed when link is clicked
+	$photoDropDownLink.on("click", function () {
+		$photoDropDown.slideToggle(250);
+	})
 
 });
